a14b7cb2e57774cb28982475118bdfceb6cdd92e
DanVelicenko <danielsvelicenko@Danielss-MacBook-Pro.local>
2020-07-29 14:07:30 +0300

    Array list, HashMaps etc.

--- src/test/java/theproject/JavaCourse.java
+++ src/test/java/theproject/JavaCourse.java
@@ -1,6 +1,8 @@
 package theproject;
 
 import javax.lang.model.element.VariableElement;
+import java.util.HashMap;
+import java.util.Map;
 
 //Java basic - variables, strings etc. (crash course)
 
@@ -15,11 +17,30 @@ public class JavaCourse {
         String stringvariable = "Thank You!!!";
                 System.out.println(stringvariable);
 
-        System.out.println(getsomeText());
+        System.out.println();
+
+        exple();
+        arrayExample();
     }
 
     private static String getsomeText() {
         return "So far so good";
+    }
+
+    private static void arrayExample(){
+        String[] carNames={"Tojota", "Nissan", "BMW", "Lada"};
+        System.out.println(carNames[2]);
+
+        carNames[2] = "Tesla";
+    }
+
+    private static void exple(){
+        Map<Integer, String> people= new HashMap<Integer, String>();
+        people.put(11, "Rembo");
+        people.put(444, "Alex");
+
+        System.out.println(people);
+
     }
 
 }
