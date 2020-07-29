f6ab56d312929e06307cb726af2c1295b1de3b7b
DanVelicenko <danielsvelicenko@Danielss-MacBook-Pro.local>
2020-07-29 13:35:27 +0300

    Changes and addition (function - date/time)

--- src/test/java/theproject/JavaCourse.java
+++ src/test/java/theproject/JavaCourse.java
@@ -1,8 +1,25 @@
 package theproject;
 
+import javax.lang.model.element.VariableElement;
+
+//Java basic - variables, strings etc. (crash course)
+
 public class JavaCourse {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        }
+        System.out.println("Hello, today date: ");
+        System.out.println(java.time.LocalDate.now());
+
+        System.out.println("And time: ");
+        System.out.println(java.time.LocalTime.now());
+
+        String stringvariable = "Thank You!!!";
+                System.out.println(stringvariable);
+
+        System.out.println(getsomeText());
     }
 
+    private static String getsomeText() {
+        return "So far so good";
+    }
+
+}
